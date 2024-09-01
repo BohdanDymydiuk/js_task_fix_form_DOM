761426e7439d9baa5ecfe6ab329170282ee1a544
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-09-01 20:54:25 +0300

    make build

---
+++ index.2b7a6a43.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("input").forEach(function(e){var t=document.createElement("label");t.className="field-label",t.setAttribute("for",e.id),t.textContent=e.name,e.placeholder=e.name[0].toUpperCase()+e.name.slice(1),e.after(t)});
+//# sourceMappingURL=index.2b7a6a43.js.map
